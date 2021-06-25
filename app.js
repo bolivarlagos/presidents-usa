fc53c87158e07d3c0141468a82c73cde44d37327
bolivarlagos <bolivarlagos@gmail.com>
2021-06-25 19:38:09 -0300

    list of USA presidents

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-const getData = async () {
-    console.log('ok')
+const rootUrl = 'https://en.wikipedia.org'
+const wikiUrl = rootUrl + '/wiki/List_of_Presidents_of_the_United_States'
+
+const getData = async () => {
+    try {
+
+        const res = await axios.get(wikiUrl)
+        const html = await res.data
+
+        let listOfPresidents = []
+        
+        const $ = cheerio.load(html)
+
+        let presidents = $('td > b > a')
+        
+        for(let president of presidents){
+            let presidentInfo = {
+                name: president.attribs.title,
+                page: rootUrl + president.attribs.href,
+                number: listOfPresidents.length + 1
+            }
+            listOfPresidents.push(presidentInfo)
+        }
+        console.log(listOfPresidents)        
+
+    } catch (error) {
+        console.log(error)        
+    }
 }
 
+getData()
