a2e0cadd98a26459dd07cd9fe66d92fb895a5f31
bolivarlagos <bolivarlagos@gmail.com>
2021-06-25 21:27:23 -0300

    add some error handling

--- server.js
+++ server.js
@@ -5,12 +5,29 @@ const app = express()
 
 app.listen(3000)
 
+app.use(express.json())
+
 app.get('/', (req, res) => {       
-    res.send('Home')
+    res.status(200).send('Home')
 })
 
 app.get('/presidents', async (req, res) => {
-    const data = await getData()    
-    res.send(data)
+    try {
+        const data = await getData()    
+        res.status(200).send(data)        
+    } catch (error) {
+        res.status(500).send({message: error})     
+    }
+})
+
+app.get('/presidents/:number', async (req, res) => {
+    try {
+        const data = await getData()
+        let presidentNum = data.filter(president => president.number == req.params.number)       
+        if(presidentNum.length === 0) throw new Error
+        res.status(200).send(presidentNum)        
+    } catch (error) {
+        res.status(500).send({message: 'This president number does not exist'})        
+    }
 })
 
